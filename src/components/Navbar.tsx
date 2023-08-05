ed26b33b869b7008dc4439bcb2bee34ad741e433
RageNeko26 <rneko2006@gmail.com>
2023-08-05 21:08:15 +0700

    feat: Add Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import { Wrapper } from "./Navbar.style"
+
+const Navbar = () => {
+    return (
+       <>
+        <Wrapper> 
+            <h2>Memo</h2>
+        </Wrapper>
+       </>
+    )
+}
+
+export default Navbar
