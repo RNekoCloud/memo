06ba78da3d3570a413f41dc0279f38cfaa432101
RageNeko26 <rneko2006@gmail.com>
2023-08-07 01:22:46 +0700

    feat: Add flex container in Task

--- src/components/Task/Task.style.ts
+++ src/components/Task/Task.style.ts
@@ -1,2 +1,11 @@
 import { styled } from "styled-components"
 
+export const TaskContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+`
+
+export const TaskItem = styled.div`
+    width: 30%;
+`
--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,12 +1,15 @@
 import Card from "../Card/Card.style"
+import { TaskContainer, TaskItem } from "./Task.style"
 
 const Task = () => {
     return(
-        <div>
-            <Card>
-                <h2>Task</h2>    
-            </Card> 
-        </div>
+        <TaskContainer>
+            <TaskItem>
+                <Card>
+                    <h2>Task</h2>    
+                </Card> 
+            </TaskItem>
+        </TaskContainer>
     )
 }
 
